fae52c284b0948b119f8109bfde0508e7a863cfc
mjrosengrant <mike@rosengrant.me>
2021-08-25 20:09:06 +0000

    Move rhodium code into separate folder

--- nanome_docking/Docking.py
+++ nanome_docking/Docking.py
@@ -5,10 +5,9 @@ from nanome.util import async_callback, ComplexUtils
 
 from smina._DockingCalculations import DockingCalculations as Smina
 from autodock4._DockingCalculationsAutodock4 import DockingCalculations as Autodock4
-from nanome_docking._DockingCalculationsRhodium import DockingCalculations as Rhodium
+from rhodium._DockingCalculationsRhodium import DockingCalculations as Rhodium
 from nanome_docking.menus._DockingMenu import DockingMenu
 from nanome_docking.menus._DockingMenuRhodium import DockingMenuRhodium
-import sys
 
 
 __metaclass__ = type
@@ -132,35 +131,3 @@ class RhodiumDocking(Docking):
         super(RhodiumDocking, self).__init__()
         self._calculations = Rhodium(self)
         self._menu = DockingMenuRhodium(self)
-
-
-def main():
-    parser = argparse.ArgumentParser(description='Parse Arguments to determine flavor of Docking to instantiate')
-    parser.add_argument('algorithm', choices=['smina', 'autodock4', 'rhodium'])
-
-    args, _ = parser.parse_known_args()
-    plugin_class = None
-    name = ''
-    algo = args.algorithm
-    if algo == "smina":
-        name = "Smina"
-        plugin_class = SminaDocking
-    elif algo == "autodock4":
-        name = "Autodock 4"
-        plugin_class = Autodock4Docking
-    elif algo == "rhodium":
-        name = "Rhodium"
-        plugin_class = RhodiumDocking
-
-    # Create the plugin, register Docking as the class to instantiate, and start listening
-    plugin_name = f'{name} Docking'
-    description = f'Run docking using {plugin_name}. Lets user choose the receptor, ligands, and diverse options'
-    category = "Docking"
-    advanced_settings = True
-    plugin = nanome.Plugin(plugin_name, description, category, advanced_settings)
-    plugin.set_plugin_class(plugin_class)
-    plugin.run()
-
-
-if __name__ == "__main__":
-    main()
--- run.py
+++ run.py
@@ -1,4 +1,35 @@
-from nanome_docking import Docking
+import argparse
+import nanome
+from nanome_docking.Docking import Autodock4Docking, RhodiumDocking, SminaDocking
 
-if __name__ == '__main__':
-    Docking.main()
+
+def main():
+    parser = argparse.ArgumentParser(description='Parse Arguments to determine flavor of Docking to instantiate')
+    parser.add_argument('algorithm', choices=['smina', 'autodock4', 'rhodium'])
+
+    args, _ = parser.parse_known_args()
+    plugin_class = None
+    name = ''
+    algo = args.algorithm
+    if algo == "smina":
+        name = "Smina"
+        plugin_class = SminaDocking
+    elif algo == "autodock4":
+        name = "Autodock 4"
+        plugin_class = Autodock4Docking
+    elif algo == "rhodium":
+        name = "Rhodium"
+        plugin_class = RhodiumDocking
+
+    # Create the plugin, register Docking as the class to instantiate, and start listening
+    plugin_name = f'{name} Docking'
+    description = f'Run docking using {plugin_name}. Lets user choose the receptor, ligands, and diverse options'
+    category = "Docking"
+    advanced_settings = True
+    plugin = nanome.Plugin(plugin_name, description, category, advanced_settings)
+    plugin.set_plugin_class(plugin_class)
+    plugin.run()
+
+
+if __name__ == "__main__":
+    main()
