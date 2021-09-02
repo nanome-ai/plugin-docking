03aca801a0f2db2491d7399a0d2deaab060684ec
mjrosengrant <mike@rosengrant.me>
2021-09-02 20:26:37 +0000

    Stop using global files

--- nanome_docking/autodock4/calculations.py
+++ nanome_docking/autodock4/calculations.py
@@ -26,21 +26,13 @@ class DockingCalculations():
         self._bond_pending = False
         self._running = False
         self.requires_site = False
-
-        self.temp_dir = tempfile.TemporaryDirectory()
-        self._protein_input = tempfile.NamedTemporaryFile(delete=False, suffix=".pdb", dir=self.temp_dir.name)
-        self._protein_input_converted = tempfile.NamedTemporaryFile(delete=False, suffix=".pdbqt", dir=self.temp_dir.name)
-        self._ligands_input = tempfile.NamedTemporaryFile(delete=False, suffix=".pdb", dir=self.temp_dir.name)
-        self._ligands_input_converted = tempfile.NamedTemporaryFile(delete=False, suffix=".pdbqt", dir=self.temp_dir.name)
-        self._ligands_output = tempfile.NamedTemporaryFile(delete=False, suffix=".pdb", dir=self.temp_dir.name)
-        self._bond_output = tempfile.NamedTemporaryFile(delete=False, suffix=".sdf", dir=self.temp_dir.name)
-        self._autogrid_input = tempfile.NamedTemporaryFile(delete=False, suffix=".gpf", dir=self.temp_dir.name)
-        self._autodock_input = tempfile.NamedTemporaryFile(delete=False, suffix=".dpf", dir=self.temp_dir.name)
-        self._autogrid_log = tempfile.NamedTemporaryFile(delete=False, suffix=".glg", dir=self.temp_dir.name)
-        self._autodock_log = tempfile.NamedTemporaryFile(delete=False, suffix=".dlf", dir=self.temp_dir.name)
         self.requires_site = False
+        self.temp_dir = tempfile.TemporaryDirectory()
+
 
     def start_docking(self, receptor, ligands, site, exhaustiveness, modes, align, replace, scoring, visual_scores, autobox):
+        temp_dir = self.temp_dir.name
+
         self._receptor = receptor
         self._ligands = ligands
         self._combined_ligands = ComplexUtils.combine_ligands(receptor, ligands)
@@ -50,88 +42,108 @@ class DockingCalculations():
         self._visual_scores = visual_scores
 
         # Save all input files
-        receptor.io.to_pdb(self._protein_input.name, pdb_options)
-        nanome.util.Logs.debug("Saved PDB", self._protein_input.name)
-        self._combined_ligands.io.to_pdb(self._ligands_input.name, pdb_options)
-        nanome.util.Logs.debug("Saved PDB", self._ligands_input.name)
+        receptor_file_pdb = tempfile.NamedTemporaryFile(delete=False, suffix=".pdb", dir=temp_dir)
+        ligands_file_pdb = tempfile.NamedTemporaryFile(delete=False, suffix=".pdb", dir=temp_dir)
+        receptor.io.to_pdb(receptor_file_pdb.name, pdb_options)
+        self._combined_ligands.io.to_pdb(ligands_file_pdb.name, pdb_options)
+        assert open(receptor_file_pdb.name).read()
+        assert open(receptor_file_pdb.name).read()
 
-        # Start docking process
-        self._prepare_receptor(self._protein_input.name, self._protein_input_converted.name)
-        self._prepare_ligands(self._ligands_input.name, self._ligands_input_converted.name)
+        # Start Ligand/ Receptor prep
+        receptor_file_pdbqt = tempfile.NamedTemporaryFile(delete=False, suffix=".pdbqt", dir=temp_dir)
+        ligands_file_pdbqt = tempfile.NamedTemporaryFile(delete=False, suffix=".pdbqt", dir=temp_dir)
+        rec_args = [
+            'conda', 'run', '-n', 'adfr-suite',
+            'prepare_receptor',
+            '-r', receptor_file_pdb.name,
+            '-o', receptor_file_pdbqt.name,
+        ]
+        subprocess.run(rec_args, cwd=temp_dir)
+        assert open(receptor_file_pdbqt.name).read()
 
-        # Prepare Grid and Docking parameters.
-        print("protein input converted name:", self._protein_input_converted.name)
+        lig_args = [
+            'conda', 'run', '-n', 'adfr-suite',
+            'prepare_ligand',
+            '-l', ligands_file_pdb.name,
+            '-o', ligands_file_pdbqt.name,
+            '-A', 'hydrogens',
+            '-v'
+        ]
+        subprocess.run(lig_args, cwd=temp_dir)
+        assert open(ligands_file_pdbqt.name).read()
+
+        # Prepare Grid parameters.
+        prepare_gpf4_script = os.path.join(os.path.dirname(__file__), 'prepare_gpf4.py')
+        autogrid_input_gpf = tempfile.NamedTemporaryFile(delete=False, suffix=".gpf", dir=temp_dir)
         grid_args = [
             'conda', 'run', '-n', 'adfr-suite',
-            'python', os.path.join(os.path.dirname(__file__), 'prepare_gpf4.py'),
-            '-l', self._ligands_input_converted.name,
-            '-r', self._protein_input_converted.name,
-            '-o', self._autogrid_input.name
+            'python', prepare_gpf4_script,
+            '-l', ligands_file_pdbqt.name,
+            '-r', receptor_file_pdbqt.name,
+            '-o', autogrid_input_gpf.name
         ]
+        subprocess.run(grid_args, cwd=temp_dir)
+        assert open(autogrid_input_gpf.name).read()
+
+        # Prepare Docking parameters
+        prepare_dpf42_script = os.path.join(os.path.dirname(__file__), 'prepare_dpf42.py')
+        autodock_input_dpf = tempfile.NamedTemporaryFile(delete=False, suffix=".dpf", dir=temp_dir)
         dock_args = [
             'conda', 'run', '-n', 'adfr-suite',
-            'python', os.path.join(os.path.dirname(__file__), 'prepare_dpf42.py'),
-            '-l', self._ligands_input_converted.name,
-            '-r', self._protein_input_converted.name,
-            '-o', self._autodock_input.name
+            'python', prepare_dpf42_script,
+            '-l', ligands_file_pdbqt.name,
+            '-r', receptor_file_pdbqt.name,
+            '-o', autodock_input_dpf.name
         ]
-
         nanome.util.Logs.debug("Prepare grid and docking parameter files")
-        subprocess.run(grid_args, cwd=self.temp_dir.name)
-        subprocess.run(dock_args, cwd=self.temp_dir.name)
-        assert open(self._autodock_input.name).read()
-        assert open(self._autogrid_input.name).read()
+        subprocess.run(dock_args, cwd=temp_dir)
+        assert open(autodock_input_dpf.name).read()
 
         # Why?
-        with open(self._autodock_input.name, 'a') as f:
-            f.write("write_all\n")
-            f.close()
+        # with open(autodock_input_dpf.name, 'a') as f:
+        #     f.write("write_all\n")
+        #     f.close()
 
         # Start Grid
-        param_filename = self._autogrid_input.name
-        full_name_log = self._autogrid_log.name
-        path = os.path.dirname(param_filename)
+        autogrid_log = tempfile.NamedTemporaryFile(delete=False, suffix=".glg", dir=temp_dir)
         args = [
             'conda', 'run', '-n', 'adfr-suite',
-            'autogrid4', '-p', param_filename, '-l', full_name_log
+            'autogrid4', '-p', autogrid_input_gpf.name, '-l', autogrid_log.name
         ]
         nanome.util.Logs.debug("Start Autogrid")
-        subprocess.run(args, cwd=path)
+        subprocess.run(args, cwd=temp_dir)
 
-        # Start Docking
-        full_name_input = self._autodock_input.name
-        path = os.path.dirname(full_name_input)
-        full_name_log = self._autodock_log.name
-
+        # Start VINA Docking
         vina_binary = os.path.join(os.path.dirname(__file__), 'vina_1.2.2_linux_x86_64')
-        dock_results = tempfile.NamedTemporaryFile(dir=self.temp_dir.name)
+        dock_results = tempfile.NamedTemporaryFile(dir=temp_dir, suffix='.pdbqt')
         args = [
             vina_binary,
-            '--receptor', self._protein_input_converted.name,
-            '--ligand', self._ligands_input_converted.name,
+            '--receptor', receptor_file_pdbqt.name,
+            '--ligand', ligands_file_pdbqt.name,
             '--center_x', '3.37',
             '--center_y', '-17.2',
             '--center_z', '8.49',
             '--size_x', '4',
             '--size_y', '4',
             '--size_z', '4',
-            '--out', dock_results
+            '--out', dock_results.name
         ]   
 
         nanome.util.Logs.debug("Start Autodock")
-        process = subprocess.run(args, cwd=path)
+        process = subprocess.run(args, cwd=temp_dir)
 
         # Start Bonds
+        nanobabel_output = tempfile.NamedTemporaryFile(dir=temp_dir, suffix=".sdf")
         nanome.util.Logs.debug("Start Bonds")
-        cmd = f'nanobabel convert -i {dock_results} -o {self._bond_output.name}'
+        cmd = f'nanobabel convert -i {dock_results.name} -o {nanobabel_output.name}'
         args = shlex.split(cmd)
-        self._nanobabel_process = subprocess.run(args, cwd=path)
+        subprocess.run(args, cwd=temp_dir)
 
         # make ligands invisible
         self.make_ligands_invisible()
 
-        docked_ligands = nanome.structure.Complex.io.from_sdf(path=self._bond_output.name)
-        nanome.util.Logs.debug("Read SDF", self._bond_output.name)
+        docked_ligands = nanome.structure.Complex.io.from_sdf(path=nanobabel_output.name)
+        nanome.util.Logs.debug("Read SDF", nanobabel_output.name)
         if len(self._combined_ligands.names) == 1:
             docked_ligands.name = self._combined_ligands.names[0] + " (Docked)"
         else:
@@ -146,46 +158,6 @@ class DockingCalculations():
         # TODO: verify this shouldn't be here anymore (test)
         # self._plugin.make_plugin_usable()
         self._plugin.add_result_to_workspace([docked_ligands])
-
-    def _prepare_ligands(self, input_file, output_file):
-        lig_args = [
-            'conda', 'run', '-n', 'adfr-suite',
-            'prepare_ligand',
-            '-l', input_file,
-            '-o', output_file,
-            '-A', 'hydrogens',
-            '-v'
-        ]
-        process = subprocess.run(lig_args, cwd=self.temp_dir.name)
-        assert open(output_file).read()
-        return process
-
-    def _prepare_receptor(self, input_file, output_file):
-        rec_args = [
-            'conda', 'run', '-n', 'adfr-suite',
-            'prepare_receptor',
-            '-r', input_file,
-            '-o', output_file,
-        ]
-        subprocess.run(rec_args, cwd=self.temp_dir.name)
-        assert open(output_file).read()
-
-    # Autogrid
-    def _start_grid(self):
-        param_filename = self._autogrid_input.name
-        full_name_log = self._autogrid_log.name
-        path = os.path.dirname(param_filename)
-
-        args = [
-            'conda', 'run', '-n', 'adfr-suite',
-            'autogrid4', '-p', param_filename, '-l', full_name_log]
-
-        nanome.util.Logs.debug("Start Autogrid")
-        self._autogrid_process = subprocess.run(args, cwd=path)
-        self._running = True
-
-    def _check_grid(self):
-        return self._autogrid_process.poll() is not None
 
     def make_ligands_invisible(self):
         for ligand in self._ligands:
