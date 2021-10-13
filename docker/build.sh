d540936133c615526d77dff518bd02cc4f6dfad2
Mike Rosengrant <mjrosengrant@users.noreply.github.com>
2021-10-12 22:57:50 -0400

    exclude autodock4 build fix (#13)
    * Fix accidental negation
    
    * Pass build args from build.sh to docker build call

--- docker/build.sh
+++ docker/build.sh
@@ -3,10 +3,12 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
+build_args=""
 cachebust=0
 while [ $# -gt 0 ]; do
   case $1 in
     -u | --update ) cachebust=1 ;;
+    *) build_args=$build_args" $1" ;;
   esac
   shift
 done
@@ -16,4 +18,4 @@ if [ ! -f ".cachebust" ] || (($cachebust)); then
 fi
 
 cachebust=`cat .cachebust`
-docker build -f Dockerfile --build-arg CACHEBUST=$cachebust -t docking:latest ..
+docker build -f Dockerfile --build-arg CACHEBUST=$cachebust $build_args -t docking:latest ..
